b70f7476e01f1278e9f2dd138b0b94773525b651
pablolucianop <pablolucianop@gmail.com>
2020-03-30 22:37:12 -0300

    Updates

---
+++ precache-manifest.c57468b7150dccacac1bead559cd051e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7616073f051578be8b5c324affab25",
+    "url": "/mascotSpelling/index.html"
+  },
+  {
+    "revision": "392bc77ee976c57ec4fb",
+    "url": "/mascotSpelling/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "31b105245d3f158c8b26",
+    "url": "/mascotSpelling/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "392bc77ee976c57ec4fb",
+    "url": "/mascotSpelling/static/js/2.178ac07d.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/mascotSpelling/static/js/2.178ac07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b105245d3f158c8b26",
+    "url": "/mascotSpelling/static/js/main.0434b073.chunk.js"
+  },
+  {
+    "revision": "345065430be9483ec733",
+    "url": "/mascotSpelling/static/js/runtime-main.69453f12.js"
+  }
+]);
