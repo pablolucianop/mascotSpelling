1c5cc8bb01451ea55b7c7638c4194a85a9cf76fb
pablolucianop <pablolucianop@gmail.com>
2020-04-17 14:16:50 -0300

    fixing

--- src/App.js
+++ src/App.js
@@ -1,74 +1,73 @@
-import React, { useState, useEffect} from 'react';
-import './App.css';
-import Child from './Child';
-import CardPoke from './CardPoke';
+import React, { useState } from 'react'
+import './App.css'
 import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Badge from 'react-bootstrap/badge';
+import Badge from 'react-bootstrap/badge'
+import Child from './Child'
+import CardPoke from './CardPoke'
 
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function BadgeConditional(props) {
-  const isLoggedIn = props.isLoggedIn.length > 0;
+  const isLoggedIn = props.isLoggedIn.length > 0
   const howMany = props.isLoggedIn.length
   if (isLoggedIn) {
-    return <Badge variant="info">{howMany}  </Badge>;
+    return <Badge variant="info">{howMany} </Badge>
   }
-  return null;
+  return null
 }
 
-
-
-
 function App() {
- 
-
   const [pokeInfoOrganized, changePokeInfoOrganized] = useState([])
   const [pokeName, changePokeName] = useState([])
-  const [stateSearch, changeStateSearch] = useState([]);
-  
+  const [stateSearch, changeStateSearch] = useState([])
 
   function searchToState(xx) {
-    
-    
     changeStateSearch(xx)
   }
 
-  React.useEffect(() => { getPokemonInfo() }, [stateSearch]);
-
+  React.useEffect(() => {
+    getPokemonInfo()
+  }, [stateSearch])
 
   function getPokemonInfo() {
+    if (stateSearch.length > 0) {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${stateSearch}/`)
+        .then(function (response) {
+          let image = JSON.stringify(
+            response.data.sprites.front_default
+          ).substring(
+            1,
+            JSON.stringify(response.data.sprites.front_default).length - 1
+          )
+          let name = JSON.stringify(response.data.name).substring(
+            1,
+            JSON.stringify(response.data.name).length - 1
+          )
+          let weight = JSON.stringify(response.data.weight)
+          let base_experience = JSON.stringify(response.data.base_experience)
 
-    if (stateSearch.length>0){
-    
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${stateSearch}/`)
-      .then(function (response) {
-        let image = JSON.stringify(response.data.sprites.front_default).substring(1, JSON.stringify(response.data.sprites.front_default).length - 1)
-        let name = JSON.stringify(response.data.name).substring(1, JSON.stringify(response.data.name).length - 1)
-        let weight = JSON.stringify(response.data.weight)
-        let base_experience = JSON.stringify(response.data.base_experience)
-
-        let arrayOfSpecs = [image, name, weight, base_experience]
-        changePokeInfoOrganized([...pokeInfoOrganized, arrayOfSpecs])
-        changePokeName( name )
-        console.log(pokeName)
-      }).catch(error => { alert(stateSearch + ' is not a pokemon') })
+          let arrayOfSpecs = [image, name, weight, base_experience]
+          changePokeInfoOrganized([...pokeInfoOrganized, arrayOfSpecs])
+          changePokeName(name)
+          console.log(pokeName)
+        })
+        .catch((error) => {
+          alert(stateSearch + ' is not a pokemon')
+        })
+    }
   }
-    
-}
   return (
     <div className="text-center">
-      <h1 >How many pokemons can you name? <BadgeConditional isLoggedIn={(pokeInfoOrganized)} /></h1>
+      <h1>
+        How many pokemons can you name?{' '}
+        <BadgeConditional isLoggedIn={pokeInfoOrganized} />
+      </h1>
       <br />
-      <Child inputSearch={searchToState}/>
-      <CardPoke pokeInfoOrganized={pokeInfoOrganized} />   
+      <Child inputSearch={searchToState} />
+      <CardPoke pokeInfoOrganized={pokeInfoOrganized} />
     </div>
-  );
+  )
 }
 
-export default App;
-
-
-
-
-
+export default App
--- src/CardPoke.js
+++ src/CardPoke.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/row';
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/row'
 
 function CardPoke(props) {
-    const a = <Row>
-
-        {props.pokeInfoOrganized.map((num) => {
-            return (
-                <div >
-                    <Card style={{ width: '10rem' }}>
-                        <Card.Img variant="top" src={num[0]} />
-                        <Card.Body>
-                            <Card.Title>{num[1]}</Card.Title>
-                            <Card.Text>
-                                <br/>
-                                <b>weight:</b>{num[2]}
-                                <br/>
-                            <b>base exp:</b>{num[3]}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
-            )
-        }
+  const a = (
+    <Row>
+      {props.pokeInfoOrganized.map((num) => {
+        return (
+          <div key={num.toString()}>
+            <Card style={{ width: '10rem' }}>
+              <Card.Img variant="top" src={num[0]} />
+              <Card.Body>
+                <Card.Title>{num[1]}</Card.Title>
+                <Card.Text>
+                  <br />
+                  <b>weight:</b>
+                  {num[2]}
+                  <br />
+                  <b>base exp:</b>
+                  {num[3]}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
         )
-        
-        }
-    </Row >
-    return ( a );
+      })}
+    </Row>
+  )
+  return a
 }
 
-export default CardPoke;
-
-
-
-
+export default CardPoke
