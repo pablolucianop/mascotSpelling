17973d0ef71f581a507fe58cf283d9a99d161750
pablolucianop <pablolucianop@gmail.com>
2020-04-20 20:21:08 -0300

    linting

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import CardPoke from './CardPoke'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function BadgeConditional(props) {
-  const isLoggedIn = props.isLoggedIn.length > 0
-  const howMany = props.isLoggedIn.length
-  if (isLoggedIn) {
-    return <Badge variant="info">{howMany} </Badge>
+function BadgeConditional(isLoggedIn) {
+  const isLoggedIno = isLoggedIn.length > 0
+  const howMany = isLoggedIn.length
+  if (isLoggedIno) {
+    return <Badge variant="info">{howMany}</Badge>
   }
   return null
 }
@@ -25,42 +25,42 @@ function App() {
     changeStateSearch(xx)
   }
 
-  React.useEffect(() => {
-    getPokemonInfo()
-  }, [stateSearch])
-
   function getPokemonInfo() {
     if (stateSearch.length > 0) {
       axios
         .get(`https://pokeapi.co/api/v2/pokemon/${stateSearch}/`)
-        .then(function (response) {
-          let image = JSON.stringify(
+        .then((response) => {
+          const image = JSON.stringify(
             response.data.sprites.front_default
           ).substring(
             1,
             JSON.stringify(response.data.sprites.front_default).length - 1
           )
-          let name = JSON.stringify(response.data.name).substring(
+          const name = JSON.stringify(response.data.name).substring(
             1,
             JSON.stringify(response.data.name).length - 1
           )
-          let weight = JSON.stringify(response.data.weight)
-          let base_experience = JSON.stringify(response.data.base_experience)
+          const weight = JSON.stringify(response.data.weight)
+          const baseExperience = JSON.stringify(response.data.baseExperience)
 
-          let arrayOfSpecs = [image, name, weight, base_experience]
+          const arrayOfSpecs = [image, name, weight, baseExperience]
           changePokeInfoOrganized([...pokeInfoOrganized, arrayOfSpecs])
           changePokeName(name)
-          console.log(pokeName)
         })
         .catch((error) => {
-          alert(stateSearch + ' is not a pokemon')
+          alert(`${stateSearch} is not a pokemon`)
         })
     }
   }
+
+  React.useEffect(() => {
+    getPokemonInfo()
+  }, [stateSearch])
+
   return (
     <div className="text-center">
       <h1>
-        How many pokemons can you name?{' '}
+        How many pokemons can you name?
         <BadgeConditional isLoggedIn={pokeInfoOrganized} />
       </h1>
       <br />
--- src/Child.js
+++ src/Child.js
@@ -1,37 +1,34 @@
-import React, { useState, Component } from 'react';
-
-import './App.css';
-import './App';
-
-
+import React, { useState, Component } from 'react'
+import './App.css'
 
 class Child extends Component {
+  handleSubmitClick2 = () => {
+    const [stateSearch, changeStateSearch] = useState([])
 
-    handleSubmitClick2 = () => {
-        const [stateSearch, changeStateSearch] = useState([])
+    const name = this.nameVal.value
+    // do something with `name`
+    changeStateSearch(name)
+    const { props: imp } = this
+    imp.inputSearch(stateSearch)
+  }
 
-        const name = this._name.value;
-        // do something with `name`
-        changeStateSearch(name)
-        this.props.inputSearch(stateSearch)
-    }
-    
-    handleSubmitClick = () => {
-        
-        const name = this._name.value;
-        // do something with `name`
+  handleSubmitClick = () => {
+    const name = this.nameVal.value
+    // do something with `name`
+    const { props: imp } = this
+    imp.inputSearch(name.toLowerCase())
+  }
 
-        this.props.inputSearch(name.toLowerCase())
-    }
-
-    render() {
-        return (
-            <div>
-                <input type="text" ref={input => this._name = input} />
-                <button onClick={this.handleSubmitClick}>check it!</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <input type="text" ref={(input) => (this.nameVal = input)} />
+        <button type="button" onClick={this.handleSubmitClick}>
+          check it!
+        </button>
+      </div>
+    )
+  }
 }
 
-export default Child;
+export default Child
